d8bba566eb17e747be26144a3d3777d2077ecb59
denom77 <157441587+denom77@users.noreply.github.com>
2024-07-08 20:16:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ client.on('ready', async () => {
     console.log(`${client.user.tag} - rich presence started!`);
 
     const r = new Discord.RichPresence()
-        .setApplicationId('1259800867385905243')
+        .setApplicationId('1259853425550692433')
         .setType('PLAYING')
-        .setURL('https://discord.gg/ZSvn6Zt5uT')
-        .setState('& Akun. Aman & Terpercaya')
-        .setName('Kei Store')
-        .setDetails('Menjual Jasa Top up, Jasa Joki')
-        .setAssetsLargeImage('https://media.discordapp.net/attachments/1190619079011225670/1259800090323980378/c49e2ffe-b8c6-4be0-82d6-5d5397df6f76.jpg?ex=668cffc3&is=668bae43&hm=ba116753676e9aa1a77b539b53ad59855d1677a14315535e60a7e3dff6743b2e&=&format=webp&width=422&height=422')
-        .setAssetsLargeText('Trusted')
-        .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1190619079011225670/1259798167516483584/vraje.gif?ex=668cfdf9&is=668bac79&hm=48a6416e5736267e0a698da7a24fad938e0f38c5aed8d7cb39e480ae1b25e1ad&')
-        .setAssetsSmallText('Verified')
-        .addButton('Join Discord Server', 'https://discord.gg/ZSvn6Zt5uT')
-        .addButton('Order? Wa', 'https://wa.me/6282132910644');
+        .setURL('https://discord.gg/warmut')
+        .setState('Ramai Player')
+        .setName('Gta-Samp Multiplayer')
+        .setDetails('Server Gta Samp Terbaik')
+        .setAssetsLargeImage('https://tenor.com/view/besos-gif-gta-san-andreas-gayy-meme-gif-25010304')
+        .setAssetsLargeText('Seru Dan Menegangkan')
+        .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1253662363501072427/1259856419201486930/wew.gif?ex=668d3439&is=668be2b9&hm=eab3b34a87bcfd3603c0cabc8e8ddc0e168ce1c1fd4e6952c2c6fa7d10d03829&')
+        .setAssetsSmallText('Join Sekarang!')
+        .addButton('DC Relative Roleplay', 'https://discord.com/invite/relativerp')
+        .addButton('Rafz Community', 'https://discord.com/invite/SuXGhD9U');
 
     client.user.setActivity(r);
     client.user.setPresence({ status: 'idle' });
