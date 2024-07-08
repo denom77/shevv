cd72c4a488163dbb0134f270201a6becb05526e8
denom77 <157441587+denom77@users.noreply.github.com>
2024-07-08 20:25:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('ready', async () => {
         .setState('Ramai Player')
         .setName('Gta-Samp Multiplayer')
         .setDetails('Server Gta Samp Terbaik')
-        .setAssetsLargeImage('https://tenor.com/view/besos-gif-gta-san-andreas-gayy-meme-gif-25010304')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1253662363501072427/1259862811601932428/image.png?ex=668d3a2d&is=668be8ad&hm=3b4b55ef3634aceba8c6703b65c7350dec9bfe83123b2f466ca6f8187a945c42&')
         .setAssetsLargeText('Seru Dan Menegangkan')
         .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1253662363501072427/1259856419201486930/wew.gif?ex=668d3439&is=668be2b9&hm=eab3b34a87bcfd3603c0cabc8e8ddc0e168ce1c1fd4e6952c2c6fa7d10d03829&')
         .setAssetsSmallText('Join Sekarang!')
