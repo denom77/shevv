d2985ecef323e583dc37380b8d73aa3d7bd29d82
denom77 <157441587+denom77@users.noreply.github.com>
2024-07-09 12:53:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('ready', async () => {
         .setState('Ramai Player')
         .setName('Gta-Samp Multiplayer')
         .setDetails('Server Gta Samp Terbaik')
-        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1253662363501072427/1259862811601932428/image.png?ex=668d3a2d&is=668be8ad&hm=3b4b55ef3634aceba8c6703b65c7350dec9bfe83123b2f466ca6f8187a945c42&')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1253662363501072427/1260111222628683858/b6ca90be-3311-4c6c-954d-f500cf71a46e.jpg?ex=668e2187&is=668cd007&hm=b5784d8be8631ece3249c951664aa0d231f674434ed2192280d5b26117de2d2c&')
         .setAssetsLargeText('Seru Dan Menegangkan')
         .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1253662363501072427/1259856419201486930/wew.gif?ex=668d3439&is=668be2b9&hm=eab3b34a87bcfd3603c0cabc8e8ddc0e168ce1c1fd4e6952c2c6fa7d10d03829&')
         .setAssetsSmallText('Join Sekarang!')
